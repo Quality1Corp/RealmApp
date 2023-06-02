cdcd563a5b8840cf76224a18321c6a5967bf803d
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-02 19:09:52 +0300

    adding storage manager template

--- RealmApp/Services/StorageManager.swift
+++ RealmApp/Services/StorageManager.swift
@@ -4,3 +4,30 @@
 //
 //  Created by MikhaiL on 31.05.2023.
 //
+
+final class StorageManager {
+    static let shared = StorageManager()
+
+    private init() {}
+    
+    // MARK: - Task List
+    func save(_ taskLists: [TaskList]) {
+        
+    }
+    
+    func save(_ taskList: String, completion: (TaskList) -> Void) {
+        
+    }
+    
+    func delete(_ taskList: TaskList) {
+        
+    }
+    
+    func edit(_ taskList: TaskList, newValue: String) {
+        
+    }
+    
+    func done(_ taskList: TaskList) {
+        
+    }
+}
