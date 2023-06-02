1feb9115d9f5c260e9576f0740ac21d47433f38c
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-02 19:08:10 +0300

    add tasks vc detail

---
+++ RealmApp/ViewControllers/TasksViewController.swift
@@ -0,0 +1,85 @@
+//
+//  TasksViewController.swift
+//  RealmApp
+//
+//  Created by MikhaiL on 01.06.2023.
+//
+
+import UIKit
+
+final class TasksViewController: UITableViewController {
+    
+    var taskList: TaskList!
+    
+    private var currentTasks: [Task] = []
+    private var completedTasks: [Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = taskList.title
+        
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonPressed)
+        )
+        navigationItem.rightBarButtonItems = [addButton, editButtonItem]
+    }
+    
+    // MARK: - UITableViewDataSource
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        section == 0 ? currentTasks.count : completedTasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "CURRENT TASKS" : "COMPLETED TASKS"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TasksCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        content.text = task.title
+        content.secondaryText = task.note
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    @objc private func addButtonPressed() {
+        showAlert()
+    }
+}
+extension TasksViewController {
+    private func showAlert(with task: Task? = nil, completion: (() -> Void)? = nil) {
+        let alertBuilder = AlertControllerBuilder(
+            title: task != nil ? "Edit Task" : "New Task",
+            message: "What do you want to do?"
+        )
+        
+        alertBuilder
+            .setTextFields(title: task?.title, note: task?.note)
+            .addAction(
+                title: task != nil ? "Update Task" : "Save Task",
+                style: .default
+            ) { [weak self] taskTitle, taskNote in
+                if let task, let completion {
+                    // TODO: - edit task
+                    return
+                }
+                self?.save(task: taskTitle, withNote: taskNote)
+            }
+            .addAction(title: "Cancel", style: .destructive)
+        
+        let alertController = alertBuilder.build()
+        present(alertController, animated: true)
+    }
+    
+    private func save(task: String, withNote note: String) {
+        
+    }
+}
