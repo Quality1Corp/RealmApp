7cee08ebf2b5bfef61a163d3f4fbfb3d8d640655
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-03 13:47:44 +0300

    Updating data model for Realm

--- RealmApp/Models/TaskList.swift
+++ RealmApp/Models/TaskList.swift
@@ -6,16 +6,17 @@
 //
 
 import Foundation
+import RealmSwift
 
-final class TaskList {
-    var title = ""
-    var date = Date()
-    var tasks: [Task] = []
+final class TaskList: Object {
+    @Persisted var title = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
 }
 
-final class Task {
-    var title = ""
-    var note = ""
-    var date = Date()
-    var isComplete = false
+final class Task: Object {
+    @Persisted var title = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
 }
